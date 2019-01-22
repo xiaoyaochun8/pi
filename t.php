a3aa0fcdb3cfd78c926047988a20e85abd8c6abc
pi <wjyzxhm888@163.com>
2019-01-22 16:43:49 +0000

    cmt

--- t.php
+++ t.php
@@ -1,4 +1,32 @@
 <?php
 
-echo 123456;
-echo 3333;
+$mysqli = new mysqli('localhost','root','123456','mydb');
+if($mysqli->connect_errno){
+    echo 'connect error!';
+    exit;
+}
+
+$insSql = 'insert into mytable values(1,"abc")';
+if(!$result = $mysqli->query($insSql)){
+    echo 'query error!';
+    exit;
+}
+
+$sql = 'select * from mytable';
+if(!$result = $mysqli->query($sql)){
+    echo 'select error!';
+    exit;
+}
+
+if($result->num_rows === 0){
+    echo 'empty set';
+    exit;
+}
+
+while($row = $result->fetch_assoc()){
+    echo 'id: '.$row['id'].' ,   name: '.$row['name']."\n";
+    var_dump($row);
+}
+
+$result->free();
+$mysqli->close();
